70b334e98bbf858d58b0ae287c978c1b77927da3
details.wes <details.wes@gmail.com>
2024-02-08 18:16:44 +0530

    initial comm

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,32 @@
-import Link from "next/link"
-
-// export const revalidate = 10 // revalidate at most every hour
+import Link from "next/link";
 
 async function getData() {
-  const res = await fetch('https://jsonplaceholder.typicode.com/posts')
-  // The return value is *not* serialized
-  // You can return Date, Map, Set, etc.
- 
-  if (!res.ok) {
-    // This will activate the closest `error.js` Error Boundary
-    throw new Error('Failed to fetch data')
-  }
- 
-  return res.json()
-}
- 
-export default async function Page() {
-  const data = await getData()
- 
-  return <main className="text-center">
+  const res = await fetch(
+    "https://645b6fe3a8f9e4d6e7686eb1.mockapi.io/api/v1/users",
+    { next: { revalidate: 1 } }
+  );
 
-     dsdda
-  </main>
+  if (!res.ok) {
+    throw new Error("Failed to fetch data");
+  }
+
+  return res.json();
+}
+
+export default async function Page() {
+  const data = await getData();
+
+  return (
+    <main className="text-center">
+      {
+        <ul>
+          {data?.map((item: any) => (
+            <Link href={`/users/${item.id}`} key={item.id}>
+              <li>{item?.name}</li>
+            </Link>
+          ))}
+        </ul>
+      }
+    </main>
+  );
 }
